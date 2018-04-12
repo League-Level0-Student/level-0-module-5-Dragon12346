b14b92552abc59c63bf0e618e9d3d1650dc28304
Dragon12346 <league@iMac.attlocal.net>
2018-04-11 18:33:08 -0700

    for loop

---
+++ src/forloop.java
@@ -0,0 +1,25 @@
+
+public class forloop {
+	public static void main(String[] args) {
+		// for (int i = 0; i <= 777; i++) {
+		// if (i%7==0) {
+		// System.out.println(i);
+		// }
+		// }
+		int q = 0;
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 3; j++) {
+				System.out.println(q + "  " + j);
+
+				
+			}
+			q = q + 1;
+		}
+		// for (int j = 0; j < 3; j++) {
+		// System.out.println(2 + " " + j);
+		// }
+
+	}
+
+}
